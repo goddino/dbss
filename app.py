4c937557b6c4f507e392aaf287b5974f40dae2e3
goddino <xlian@dolby.com>
2025-07-14 07:57:07 +0000

    Added Groq

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request
+from groq import Groq
+import os
+
 import joblib
+
+#https://console.groq.com/keys
+groq_api = os.environ.get('groq')
+if groq_api == None:
+    groq_api = ""
+
+os.environ['GROQ_API_KEY'] = groq_api
+client = Groq()
 
 app = Flask(__name__)
 
@@ -9,15 +20,16 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    q = request.form.get("q")
-
     # db
     return(render_template("main.html"))
            
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
-    q = request.form.get("q")
     return(render_template("dbs.html"))
+           
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
            
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
@@ -30,6 +42,22 @@ def prediction():
     pred = model.predict([[q]])
 
     return(render_template("prediction.html",r=pred[0][0]))
+           
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+
+    # load model
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
            
 if __name__ == "__main__":
     app.run()
